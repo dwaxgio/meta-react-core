9ac4d3e37b1bc7a2d1d90b8244595df154297e0a
edward <edward_gio@hotmail.com>
2023-04-10 11:53:16 -0500

    Heading component created and imported in app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-
-function Header() {
-  return <h1>Header rendered from component</h1>;
-}
+import Heading from "./Heading";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Heading />
     </div>
   );
 }
---
+++ src/Heading.js
@@ -0,0 +1,5 @@
+function Heading() {
+  return <h1>Header rendered from component</h1>;
+}
+
+export default Heading;

3b8b519154fea62886fb2a0d6972cd0d8a3038e0
edward <edward_gio@hotmail.com>
2023-04-11 11:52:52 -0500

    Arrow Function for Heading component

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -10,15 +10,26 @@
 
 //
 
-//The same function, but as a Function Expression
-const Heading = function (props) {
+// //The same function, but as a Function Expression
+// const Heading = function (props) {
+//   const styles = {
+//     background: "azure",
+//     color: "tomato",
+//     fontSize: "40px",
+//   };
+
+//   return <h1 style={styles}>Hello, {props.firstName}</h1>;
+// };
+
+//The same function, but as an Arrow Function
+const Heading = (props) => {
   const styles = {
     background: "azure",
     color: "tomato",
     fontSize: "40px",
   };
 
-  return <h1 style={styles}>Hello, {props.firstName}</h1>;
+  return <h1 style={styles}>Hello there, {props.firstName}</h1>;
 };
 
 export default Heading;

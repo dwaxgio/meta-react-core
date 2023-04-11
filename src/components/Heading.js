1cba106e8e69f12809afe31580c9c566cfd55d8b
edward <edward_gio@hotmail.com>
2023-04-11 16:24:26 -0500

    Function call added for Heading component

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -29,7 +29,20 @@ const Heading = (props) => {
     fontSize: "40px",
   };
 
-  return <h1 style={styles}>Hello there, {props.firstName}, {props.firstName == "Dwaxgio" ? "Dwaxgio is here" : ""}</h1>;
+  const randomNumber = () => Math.floor(Math.random() * 10) + 1;
+
+  return (
+    <div>
+      <h1 style={styles}>
+        Hello there, {props.firstName},{" "}
+        {props.firstName == "Dwaxgio" ? "Dwaxgio is here" : ""}
+      </h1>
+      <h2>
+        Here is a random number from 0 to 10 using funcion call in jsx:{" "}
+        {randomNumber()}
+      </h2>
+    </div>
+  );
 };
 
 export default Heading;

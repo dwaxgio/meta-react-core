b419e90b34d3a99856181d3c18f85b5519176c74
edward <edward_gio@hotmail.com>
2023-04-12 09:29:12 -0500

    Expressions as props added to Expressions component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Heading from "./components/Heading";
+import Expressions from "./components/Expressions";
+
+const bool = false;
+const str1 = "just";
 
 function App() {
   return (
     <div className="App">
       <Heading firstName="Dwaxgio" />
       <Heading firstName="Ed" />
+      <Expressions
+        toggleBoolean={!bool}
+        math={(10 + 20) / 3}
+        str={str1 + " another " + "string"}
+      />
     </div>
   );
 }

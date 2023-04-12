dca42853a37e432840d665161346c7b6171d32d5
edward <edward_gio@hotmail.com>
2023-04-12 11:03:48 -0500

    Card component added to display cards in app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Heading from "./components/Heading";
 import Expressions from "./components/Expressions";
 import Attributes from "./components/Attributes";
+import Card from "./components/Card";
 
 const bool = false;
 const str1 = "just";
@@ -17,7 +18,13 @@ function App() {
         math={(10 + 20) / 3}
         str={str1 + " another " + "string"}
       />
-      <Attributes/>
+      <Attributes />
+      <div>
+        <h1>Task: Add three card elements</h1>
+        <Card h2="First card's h2" h3="First card's h3"/>
+        <Card h2="Second card's h2" h3="Second card's h3"/>
+        <Card h2="Third card's h2" h3="Third card's h3"/>
+      </div>
     </div>
   );
 }

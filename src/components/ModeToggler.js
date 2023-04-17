cce961337c4126ec09f07c739b6e7bf266a94925
edward <edward_gio@hotmail.com>
2023-04-17 11:05:37 -0500

    ModeToggler component added to to handle user events

--- src/components/ModeToggler.js
+++ src/components/ModeToggler.js
@@ -3,19 +3,40 @@ const ModeToggler = () => {
   const darkMode = <h1>Dark Mode is On</h1>;
   const lightMode = <h1>Light Mode is On</h1>;
 
+  const modeText = "";
+  const modeTextFunction = () => {
+    if (darkModeOn) {
+      modeText = <h1>Dark Mode is On</h1>;
+    } else {
+      <h1>Light Mode is On</h1>;
+    }
+  };
+
   const handleClick = () => {
     darkModeOn = !darkModeOn;
 
     if (darkModeOn) {
       console.log("Dark Mode is On");
+    //   modeText = <h1>Dark Mode is On</h1>;
     } else {
       console.log("Light Mode is On");
+    //   modeText = <h1>Light Mode is On</h1>;
     }
   };
 
   return (
     <div>
       {darkModeOn ? darkMode : lightMode};
+      {/* <button
+        onClick={() => {
+          handleClick;
+          modeTextFunction;
+        }}
+      >
+        Click me for dark or light mode
+      </button>
+       */}
+       {/* {modeText} */}
       <button onClick={handleClick}>Click me for dark or light mode</button>
     </div>
   );
